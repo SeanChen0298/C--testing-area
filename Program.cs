be064175efa796698d7471a2da652229f4a9f724
SeanChen0298 <yoonsean0298@gmail.com>
2021-11-19 21:03:27 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TestArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!\n");
+
+            RomanToInteger romanToInteger = new RomanToInteger();
+            Console.WriteLine($"III is {romanToInteger.RomanToInt("III")}");
+            Console.WriteLine($"IV is {romanToInteger.RomanToInt("IV")}");
+            Console.WriteLine($"IX is {romanToInteger.RomanToInt("IX")}");
+            Console.WriteLine($"LVIII is {romanToInteger.RomanToInt("LVIII")}");
+            Console.WriteLine($"MCMXCIV is {romanToInteger.RomanToInt("MCMXCIV")}");
+        }
+    }
+}
+
+
---
+++ RomanToInteger.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+
+namespace TestArea
+{
+    class RomanToInteger
+    {
+        /// <summary>
+        /// Converts Roman characters ('I', 'V', 'X', 'L', 'C', 'D', 'M') into numbers.
+        /// </summary>
+        public int RomanToInt(string s)
+        {
+            // Convert string into char array. III -> ["I","I","I"]
+            char[] charArray = s.ToCharArray();
+
+            // Convert each element in char array into integer using RomanNumbers Enum.
+            List<int> list = new List<int>();
+            foreach (char ch in charArray)
+            {
+                var number = (int)Enum.Parse(typeof(RomanNumbers), ch.ToString());
+                list.Add(number);
+            }
+
+            // Loops through the int list, add if next <= current, subtract if next > current. 
+            int result = 0;
+            for (int i = 0; i < list.Count - 1; i++)
+            {
+                if (i == list.Count)
+                {
+                    result += list[i];
+                }
+                else
+                {
+                    if (list[i] >= list[i + 1])
+                    {
+                        result += list[i];
+                    }
+                    else if (list[i] < list[i + 1])
+                    {
+                        result -= list[i];
+                    }
+                }
+            }
+
+            // Add in the last element of int list.
+            return result += list[list.Count-1];
+        }
+    }
+
+    enum RomanNumbers
+    {
+        I = 1,
+        V = 5,
+        X = 10,
+        L = 50,
+        C = 100,
+        D = 500,
+        M = 1000
+    }
+}
